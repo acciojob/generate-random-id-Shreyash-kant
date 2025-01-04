d77be4b6862b267d5cc210f16f35cd35580681df
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-01-04 18:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	const str = "ABCDEFGHIJKLMNOPQRSTYVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let result = "";
+	for(let i=0;i<l;++i){
+		result+= str[Math.floor(Math.random()*62)];
+	}
+	return result;
 }
 
 // Do not change the code below.
